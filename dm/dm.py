e5bc93e25fa2e251b005a63cdab67168bc205fd7
Firesuiry <4690734@qq.com>
2019-08-08 19:12:48 +0800

    完成命令传输模块 完成命令执行模块
    Signed-off-by: Firesuiry <4690734@qq.com>

--- dm/dm.py
+++ dm/dm.py
@@ -1,25 +1,136 @@
 import win32com.client
 import time
+import os,json
+
+class dmBase():
+    def __init__(self):
+        self.id = 0
 
 
-dm = win32com.client.Dispatch('dm.dmsoft')  #调用大漠插件
-print(dm.ver())#输出版本号
-hwnds = dm.EnumWindow(0,"League of Legends (TM) Client - [Windows 7 x64]","",1+4+8+16)
-print(hwnds)
-hwnd = int(hwnds)
+    def getCommand(self):
+        f_path = r'E:\develop\autoLOL\dm\data\{}.txt'.format(self.id)
+        command = ''
+        if os.path.exists(f_path):
+            with open(f_path) as f:
+                command = f.read()
+        print(command)
+        return command
 
-dm_ret = dm.BindWindowEx(hwnd, "gdi", "windows", "windows", "", 0)
-print('结果：%s'%dm_ret)
-start = time.time()
-last = 0
-i = 0
-while True:
-    if time.time() - last > 0.5:
-        i += 1
-        dm_ret = dm.Capture(0,0,2000,2000,"ans/screen%s.bmp"%i)
-        print('第%s张 结果：%s'%(i,dm_ret))
-        last = time.time()
-print('经过的时间：%s'%(time.time()-start))
 
-dm_ret = dm.UnBindWindow()
-print('结果：%s'%dm_ret)
+class dmOperater(dmBase):
+    def __init__(self,id,hwnd,manager):
+        self.id = id
+        self.hwnd = hwnd
+        self.manager = manager
+        print ('dmOperater启动，id：{}'.format(id))
+        self.dm = win32com.client.Dispatch('dm.dmsoft')  # 调用大漠插件
+        dm_ret = self.dm.SetShowErrorMsg(0)
+        dm_ret = self.dm.BindWindowEx(hwnd, "gdi", "windows", "windows", "", 0)
+        print('窗口绑定结果：%s'%dm_ret)
+        self.commandCahe = ''
+
+    def mainExcuter(self):
+        command = self.getCommand()
+        dm_ret = self.dm.Capture(0, 0, 2000, 2000, "screen%s/0.bmp" % self.id)
+        self.excuteCommand(command)
+
+    def excuteCommand(self,command):
+        if command == '' or command == self.commandCahe:
+            return
+        self.commandCahe == command
+        cDict = json.loads(command)
+
+        commandList = cDict['commandList']
+        for commandDict in commandList:
+            #执行命令模块
+            print (commandDict)
+            key = commandDict.get('key','')
+            x = commandDict.get('x','')
+            y = commandDict.get('y', '')
+            delay = commandDict.get('delay',100)
+            name = commandDict.get('name', '')
+            if name == '':
+                continue
+            mathod = 'self.dm.{}'.format(name)
+            args = ''
+            for arg in [key,x,y]:
+                if arg != '':
+                    if args == '':
+                        args += '('
+                    else:
+                        args += ','
+                    args += '\'' + str(arg) + '\''
+            if args == '':
+                args = '()'
+            else:
+                args += ')'
+            command = mathod + args
+
+            try:
+                print ('excute command:{}'.format(command))
+                eval(command)
+                time.sleep(delay/1000)
+            except Exception as e:
+                print(e)
+            finally:
+                pass
+
+
+
+    def mainLoop(self):
+        pass
+
+class dmManager(dmBase):
+    def __init__(self):
+        self.id = 0
+        self.dm = win32com.client.Dispatch('dm.dmsoft')  #调用大漠插件
+        print ('dmManager，id：{}'.format(self.id))
+        self.operaterHwndsList = []
+        self.operaterDict = {}
+        self.opId = 0 #为了创建operater储存id
+
+    def checkHwnd(self):
+        hwnds = dm.EnumWindow(0, "League of Legends (TM) Client - [Windows 7 x64]", "", 1 + 4 + 8 + 16)
+        for hwnd in hwnds:
+            if hwnd not in self.operaterHwndsList:
+                self.opId += 1
+                op = dmOperater(self.opId,hwnd,self) #此处待开发多线程方式
+                opDict = {
+                    'id':self.opId,
+                    'hwnd':hwnd,
+                    'op':op
+                }
+                self.operaterHwndsList.append(hwnd)
+                self.operaterDict[hwnd] = opDict
+
+
+
+
+
+if __name__ == '__main__':
+    p = dmOperater(1024,0,None)
+    p.mainExcuter()
+
+
+    exit()
+    dm = win32com.client.Dispatch('dm.dmsoft')  #调用大漠插件
+    print(dm.ver())#输出版本号
+    hwnds = dm.EnumWindow(0,"League of Legends (TM) Client - [Windows 7 x64]","",1+4+8+16)
+    print(hwnds)
+    hwnd = int(hwnds)
+
+    dm_ret = dm.BindWindowEx(hwnd, "gdi", "windows", "windows", "", 0)
+    print('结果：%s'%dm_ret)
+    start = time.time()
+    last = 0
+    i = 0
+    while True:
+        if time.time() - last > 0.5:
+            i += 1
+            dm_ret = dm.Capture(0,0,2000,2000,"ans/screen%s.bmp"%i)
+            print('第%s张 结果：%s'%(i,dm_ret))
+            last = time.time()
+    print('经过的时间：%s'%(time.time()-start))
+
+    dm_ret = dm.UnBindWindow()
+    print('结果：%s'%dm_ret)
--- operater.py
+++ operater.py
@@ -1,13 +1,88 @@
 import time
-import win32gui, win32ui, win32con, win32api
+
+import json,os
 
 class operater():
-    def __init__(self):
-        pass
+    def __init__(self,id = 0):
+        self.id = id
+        self.commandCahe = {
+            'time':0,
+            'commandList':[]
+        }
+
+    def sendCommand(self):
+        f_path = r'E:\develop\autoLOL\dm\data\{}.txt'.format(self.id)
+        self.commandCahe['time'] = time.time()
+        command = json.dumps(self.commandCahe)
+        self.commandCahe = {
+            'time':0,
+            'commandList':[]
+        }
+        with open(f_path,'w') as f:
+            command = f.write(command)
+
+
+
+    def addKeyboardCommandToJson(self,keyChar,delay = 100,Down = False,Up = False):
+        mathodName = 'KeyPressChar'
+        if Down:
+            mathodName = 'KeyDownChar'
+        if Up:
+            mathodName = 'KeyUpChar'
+
+        print('addKeyboardCommandToJson mathod:{} key:{} delay={}'.format(mathodName,keyChar,delay))
+        command = {
+            'name':mathodName,
+            'key':keyChar,
+            'delay':delay
+        }
+        self.commandCahe['commandList'].append(command)
+
+
+    def addMouseCommandToJson(self,x = -1,y = -1,liftClick = False,rightClick = False,delay = 100):
+        '''
+        :param x: -1==NoMove
+        :param y: -1==NoMove
+        :param liftClick:
+        :param rightClick:
+        :param delay:
+        :return:
+        '''
+        if x != -1 and y != -1:
+            mathodName = 'MoveTo'
+            command = {
+                'name':mathodName,
+                'x':x,
+                'y':y,
+                'delay':delay
+            }
+            self.commandCahe['commandList'].append(command)
+
+        mathodName = ''
+        if liftClick:
+            mathodName = 'LeftClick'
+        if rightClick:
+            mathodName = 'RightClick'
+        if mathodName == '':
+            return
+        command = {
+            'name':mathodName,
+            'delay':delay
+        }
+        self.commandCahe['commandList'].append(command)
+
+
+
 
 
 
 if __name__ == "__main__":
-    pass
+    p = operater(1024)
+    p.addKeyboardCommandToJson('ctrl',Down=True)
+    p.addKeyboardCommandToJson('w')
+    p.addKeyboardCommandToJson('ctrl',Up=True)
+    p.addMouseCommandToJson(1000,1000,liftClick=True)
+    p.sendCommand()
+
 
 
