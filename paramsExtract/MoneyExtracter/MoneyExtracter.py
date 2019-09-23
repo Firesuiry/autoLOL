4e93066c734b8c997a83690e66bd8da48206ced0
ShiroShibe <1196768261@qq.com>
2019-09-23 21:33:07 +0800

    添加了金钱判定

---
+++ paramsExtract/MoneyExtracter/MoneyExtracter.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+from typing import Any
+import tensorflow as tf
+def RemoveZero(det: np.array) -> np.array:
+    """去除上下的空元素"""
+    h, w = det.shape[:2]
+    need_del = []
+    for i in range(h):
+        if np.all(det[i, :] == 0):
+            need_del.append(i)
+    det = np.delete(det, need_del, axis=0)
+    return det
+def get_charter(self:Any,pic:np.array) -> int:
+    """通过取阈值来得到的二值化图像，再通过是否存在像素点来分割图像"""
+    """返回值:分割图像的numpy数组"""
+    try:
+        img = cv2.cvtColor(pic, cv2.COLOR_RGB2GRAY)
+        _, det = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+        det = RemoveZero(det)
+        pre_value = True
+        need_slice = []
+        h, w = det.shape[:2]
+        for i in range(w):
+            if pre_value != np.all(det[:, i] == 0):
+                pre_value = np.all(det[:, i] == 0)
+                need_slice.append(i)
+        ret = []
+        while need_slice:
+            k = need_slice.pop(0)
+            e = need_slice.pop(0)
+            done_array = det[:, k:e]
+            w_add, h_add = 12 - done_array.shape[0], 7 - done_array.shape[1]
+            ret.append(np.pad(done_array, ((w_add, 0), (0, h_add)), "constant", constant_values=(0, 0)))
+        ll = self.ai.useModel(self, "Moeny.h5", ret).tolist()
+        p = ""
+        for i in ll:
+            p += i
+        return int(p)
+    except Exception as e:
+        print(e)
+        return -1
+if __name__ == '__main__':
+    model = tf.keras.models.load_model(r'C:\Users\Administrator\Desktop\autoLOL\model\Money.h5')
+    test_images = get_charter("C:\\Users\\Administrator\\Desktop\\ans\\MONEY13.png")
+    predictions = model.predict(np.array(test_images))
+    y = np.argmax(predictions, axis=1)
+    print(y)
