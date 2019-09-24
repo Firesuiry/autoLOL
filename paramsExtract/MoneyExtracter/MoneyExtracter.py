690114dcfc9f894506c5314b0b2e3921a595eae4
ShiroShibe <1196768261@qq.com>
2019-09-24 14:42:09 +0800

    修改了一下金钱判定

--- paramsExtract/MoneyExtracter/MoneyExtracter.py
+++ paramsExtract/MoneyExtracter/MoneyExtracter.py
@@ -15,10 +15,9 @@ def RemoveZero(det: np.array) -> np.array:
 	return det
 
 
-def get_charter(self: Any, pic: np.array) -> int:
+def get_charter(self:Any,pic: np.array) -> int:
 	"""通过取阈值来得到的二值化图像，再通过是否存在像素点来分割图像"""
 	"""返回值:分割图像的numpy数组"""
-
 	img = cv2.cvtColor(pic, cv2.COLOR_RGB2GRAY)
 	_, det = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 	det = RemoveZero(det)
@@ -36,20 +35,13 @@ def get_charter(self: Any, pic: np.array) -> int:
 		done_array = det[:, k:e]
 		w_add, h_add = 12 - done_array.shape[0], 7 - done_array.shape[1]
 		ret.append(np.pad(done_array, ((w_add, 0), (0, h_add)), "constant", constant_values=(0, 0)))
-	ret = np.array(ret)
-	if ret.shape == (12,7):
-		ret.reshape(1,12,7)
-	ll = self.ai.useModel("Money.h5", ret).tolist()
+	ll = np.argmax(self.ai.useModel("Money.h5", np.array(ret)), axis=1).tolist()
 	p = ""
 	for i in ll:
-		p += i
+		p += str(i)
 	return int(p)
-
-
-
 if __name__ == '__main__':
-	model = tf.keras.models.load_model(r'C:\Users\Administrator\Desktop\autoLOL\model\Money.h5')
-	test_images = get_charter("C:\\Users\\Administrator\\Desktop\\ans\\MONEY13.png")
-	predictions = model.predict(np.array(test_images))
-	y = np.argmax(predictions, axis=1)
-	print(y)
+	model = tf.keras.models.load_model(r'C:\Users\Administrator\Desktop\vr\autoLOL\autoLOL\model\Money.h5')
+	pic = cv2.imread(r"C:\Users\Administrator\Desktop\ans\MONEY10.png")
+	test_images = get_charter(pic)
+	print(test_images)
